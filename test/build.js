bcababf616f007e688f6d5aa32c579d231679cb1
Nakayama Yuhei <nakayama-yuhei@kayac.com>
2015-10-31 21:34:50 +0900

    add simple test

---
+++ test/build.js
@@ -0,0 +1,28 @@
+"use strict";
+
+let fs = require("fs");
+let tap = require("tap");
+
+let statTest = (t) => {
+  return (err, stat) => {
+    t.ok(!err, "file exists");
+    t.ok(stat.size > 0, "file size bigger than 0");
+    t.end();
+  };
+};
+
+tap.test("can build index.html", (t) => {
+  fs.stat("public/index.html", statTest(t));
+});
+
+tap.test("can build index.css", (t) => {
+  fs.stat("public/css/index.css", statTest(t));
+});
+
+tap.test("can build index.js", (t) => {
+  fs.stat("public/js/index.js", statTest(t));
+});
+
+tap.test("can build modernizr.js", (t) => {
+  fs.stat("public/js/lib/modernizr.js", statTest(t));
+});
