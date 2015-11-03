3a575b3734dee296760a7eb0b69c8653d27ab20c
Nakayama Yuhei <nakayama-yuhei@kayac.com>
2015-11-03 18:49:07 +0900

    build.coffee -> build.js

---
+++ gulp/task/build.js
@@ -0,0 +1,21 @@
+const gulp = require("gulp");
+const config = require("../config");
+
+const runSequence = require("run-sequence");
+
+const copy = [
+  "copy",
+  "modernizr",
+];
+
+const compile = [
+  "jade",
+  "stylus",
+  "coffeeify",
+];
+
+gulp.task("build", () => {
+  // some tasks (ex. embeded data-url in stylus) depend on compiled static files
+  // so we need to separate them!
+  runSequence(["clean"], copy, compile);
+});
