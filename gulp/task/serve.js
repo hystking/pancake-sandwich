7ce9e698df1cb4e8c758354dfdc1cdf326e69698
Nakayama Yuhei <nakayama-yuhei@kayac.com>
2015-11-09 11:49:09 +0900

    serve.coffee -> serve.js

---
+++ gulp/task/serve.js
@@ -0,0 +1,15 @@
+const gulp = require("gulp");
+const $ = require("gulp-load-plugins")();
+const config = require("../config");
+
+const connect = require("connect");
+const serveStatic = require("serve-static");
+const livereload = require("connect-livereload");
+
+gulp.task("serve", () => {
+  $.livereload.listen();
+  connect()
+    .use(livereload())
+    .use(serveStatic(config.dest))
+    .listen(9000);
+});
