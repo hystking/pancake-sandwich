12813387e364c6f631684121cccca22d8d2564b3
Nakayama Yuhei <nakayama-yuhei@kayac.com>
2015-11-09 21:27:36 +0900

    copy.coffee -> copy.js

---
+++ gulp/task/copy.js
@@ -0,0 +1,17 @@
+const gulp = require("gulp");
+const $ = require("gulp-load-plugins")();
+const config = require("../config");
+
+gulp.task("copy", () => {
+  const pngquant = require("imagemin-pngquant");
+  gulp
+    .src([
+      `${config.src}/img/**/*.{png,jpg,gif}`,
+      `!${config.src}/img/sprite{,/**}`,
+    ], {base: config.src})
+    .pipe($.if(!config.isDebug, $.imagemin({
+        progressive: true,
+        use: [pngquant()],
+    })))
+    .pipe(gulp.dest(config.dest));
+});
