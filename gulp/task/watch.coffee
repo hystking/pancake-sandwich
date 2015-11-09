2abd710fa71204f06dfd23fb064488842de08cd4
Nakayama Yuhei <nakayama-yuhei@kayac.com>
2015-11-09 21:41:22 +0900

    watch.coffee -> watch,js

--- gulp/task/watch.coffee
+++
@@ -1,7 +0,0 @@
-gulp = require "gulp"
-config = require "../config"
-
-gulp.task "watch", ->
-  gulp.watch ["#{config.src}/jade/**/*"], ["jade"]
-  gulp.watch ["#{config.src}/stylus/**/*"], ["stylus"]
-  gulp.watch ["#{config.src}/coffee/**/*"], ["coffeeify"]
---
+++ gulp/task/watch.js
@@ -0,0 +1,8 @@
+const gulp = require("gulp");
+const config = require("../config");
+
+gulp.task("watch", () => {
+  gulp.watch([`${config.src}/jade/**/*`], ["jade"]);
+  gulp.watch([`${config.src}/stylus/**/*`], ["stylus"]);
+  gulp.watch([`${config.src}/coffee/**/*`], ["coffeeify"]);
+});
